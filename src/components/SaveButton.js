77a6ec87e4640e1860cde2538e259548bb6fd5f6
kubaadamczyk <poczta@kubaadamczyk.pl>
2022-10-04 19:29:22 +0000

    kolorowy przyicsk zapisz

--- src/components/SaveButton.js
+++ src/components/SaveButton.js
@@ -1,8 +1,10 @@
 export default function SaveButton(props) {
   return (
-    <div className="save-button">
-      <div className="conti">
-        <button onClick={props.saveDeck}>Zapisz</button>
+    <div className="save-button bottom-buttons">
+      <div className="cont">
+        <button className="save" onClick={props.saveDeck}>
+          Zapisz talię
+        </button>
       </div>
     </div>
   );
