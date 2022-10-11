9db411d1dbd9a529087d8b4f7da10d1703dc0c3b
kubaadamczyk <poczta@kubaadamczyk.pl>
2022-10-11 13:07:23 +0000

    skuteczne powtórki

--- src/components/Lern.js
+++ src/components/Lern.js
@@ -3,17 +3,17 @@ import { useState, useEffect } from "react";
 import { readDeckFromFile } from "./FilesEditor.js";
 
 export default function Lern(props) {
-  const [deck, setDeck] = useState(false);
-  const [counter, setCounter] = useState(0);
-  const [frontSide, setFrontSide] = useState(true);
-  const [toRepeat, setToRepeat] = useState([]);
+  const [rootDeck, setRootDeck] = useState(false);
   const [repeatCounter, setRepeatCounter] = useState(0);
+  const [toRepeat, setToRepeat] = useState("");
 
   useEffect(() => {
     readDeckFromFile("test").then(
       (resolve) => {
         const loadedDeck = JSON.parse(resolve);
-        setDeck(loadedDeck);
+        setRootDeck(loadedDeck);
+        setRepeatCounter(0);
+        setToRepeat([]);
       },
       (error) => {
         props.openStatement({
@@ -25,88 +25,95 @@ export default function Lern(props) {
     );
   }, []);
 
+  function endRound(newToRepeat) {
+    props.openStatement({
+      status: "success",
+      text: `Dziś przerobiłeś ${rootDeck.length} słów, nie wiedziałeś ${newToRepeat.length}`,
+    });
+
+    if (newToRepeat.length === 0) {
+      props.choosePage(false);
+    } else {
+      setToRepeat(newToRepeat);
+      setRepeatCounter((prev) => ++prev);
+    }
+  }
+
+  if (rootDeck) {
+    const deck =
+      toRepeat.length > 0
+        ? rootDeck.filter((item) => {
+            return toRepeat.includes(item.id);
+          })
+        : rootDeck;
+    return (
+      <LerningRound deck={deck} deckLength={deck.length} endRound={endRound} />
+    );
+  } else {
+    return "loading...";
+  }
+}
+
+function LerningRound(props) {
+  const [counter, setCounter] = useState(0);
+  const [frontSide, setFrontSide] = useState(true);
+  const [toRepeat, setToRepeat] = useState([]);
+
+  const { deck, deckLength, endRound } = props;
+
   function rotateCard() {
     setFrontSide(false);
   }
 
   function nextCard(addCardToRepeat) {
-    const cardsQuantity = deck.length;
     const newCounter = counter + 1;
-    let toRepeatQuantity = toRepeat.length;
+    let newToRepeatArray = toRepeat;
+    console.log(newToRepeatArray);
+    setFrontSide(true);
 
     if (addCardToRepeat) {
-      toRepeatQuantity++;
-      setToRepeat((prevAddCardToRepeat) => [
-        ...prevAddCardToRepeat,
-        deck[counter].id,
-      ]);
+      newToRepeatArray.push(deck[counter].id);
+      console.log(newToRepeatArray, "powiększ");
     }
 
-    if (newCounter < cardsQuantity) {
+    if (newCounter < deckLength) {
       setCounter(newCounter);
-      setFrontSide(true);
+      setToRepeat(newToRepeatArray);
     } else {
-      props.openStatement({
-        status: "success",
-        text: `Dziś przerobiłeś ${cardsQuantity} słów, nie wiedziałeś ${toRepeatQuantity}`,
-      });
-
-      if (toRepeatQuantity > 0 && repeatCounter <= 3) {
-        setFrontSide(true);
-        setRepeatCounter((prev) => ++prev);
-        setDeck((prevDeck) => {
-          let newDeck = prevDeck;
-          //   const newDeck = prevDeck.map((item) => {
-          //     if (toRepeat.includes(item.id)) {
-          //       return item;
-          //     }
-          //   });
-          //   console.log(newDeck);
-          console.log("wy");
-          setToRepeat([]);
-          // return newDeck;
-        });
-        setCounter(0);
-      } else {
-        props.choosePage(false);
-      }
+      setCounter(0);
+      setToRepeat([]);
+      endRound(newToRepeatArray);
     }
   }
 
   return (
     <div className="lerning">
-      {deck && counter < deck.length ? (
-        <>
-          <div className="cont card-container">
-            <LerningCard frontSide={frontSide} data={deck[counter]} />
-          </div>
-          <LerningButtons
-            frontSide={frontSide}
-            rotateCard={rotateCard}
-            nextCard={nextCard}
-          />
-        </>
-      ) : (
-        "loading..."
-      )}
+      <div className="cont card-container">
+        <LerningCard frontSide={frontSide} data={deck[counter]} />
+      </div>
+      <LerningButtons
+        frontSide={frontSide}
+        rotateCard={rotateCard}
+        nextCard={nextCard}
+      />
     </div>
   );
 }
 
 function LerningCard(props) {
   if (props.frontSide) {
-    return <CardFront data={props.data.pl} />;
+    return <CardFront pl={props.data.pl} />;
   } else {
-    return <CardBack data={props.data.en} />;
+    return <CardBack en={props.data.en} />;
   }
 }
 
 function CardFront(props) {
-  return <div className="lerning-card front">{props.data}</div>;
+  return <div className="lerning-card front">{props.pl}</div>;
 }
 
 function CardBack(props) {
-  return <div className="lerning-card back">{props.data}</div>;
+  return <div className="lerning-card back">{props.en}</div>;
 }
 
 function LerningButtons(props) {
